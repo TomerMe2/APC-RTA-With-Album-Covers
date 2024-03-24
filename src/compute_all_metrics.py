fix test recos computation bug

src/compute_all_metrics.py
@@ -7,49 +7,44 @@ from src.plot_results import confidence_interval
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--recos_path", type=str, required=False,
-                        help="path to recos", default="resources/recos")
-    parser.add_argument("--plots_path", type=str, required=False,
-                        help="path to plots", default="resources/plots")
-    parser.add_argument("--models", type=str, required=False,
-                        help="comma separated names of models to evaluate",
-                        default="SKNN,VSKNN,STAN,VSTAN,MF-AVG,MF-CNN,MF-GRU,MF-Transformer,FM-Transformer,NN-Transformer")
+    parser.add_argument("--recos_paths", type=str, required=False,
+                        help="comma separated paths to recos")
     parser.add_argument('--data_manager_path', type=str, required=True,
                         help='Path to the data manager data, the folder should "embeddings" and "data_split" folders.')
 
     args = parser.parse_args()
-    model_names = args.models.split(",")
-    l = len(model_names)
-    recos = [np.load(("%s/%s.npy") % (args.recos_path, m)) for m in model_names]
+    recos_paths = args.recos_paths.split(",")
+
+    recos = [np.load(path) for path in recos_paths]
     data_manager = DataManager(foldername=args.data_manager_path)
     test_evaluator, test_dataloader = data_manager.get_test_data("test")
 
-    for model_name, rec in zip(model_names, recos):
-        print('Computing metrics for model:', model_name)
-
-        recall = test_evaluator.compute_all_recalls(rec)
-        recall_ci = confidence_interval(recall)
-        print(f'Recall: {np.mean(recall)}+-{recall_ci}')
-
-        ndcg = test_evaluator.compute_all_ndcgs(rec)
-        ndcg_ci = confidence_interval(ndcg)
-        print(f'NDCG: {np.mean(ndcg)}+-{ndcg_ci}')
-
-        clicks = test_evaluator.compute_all_clicks(rec)
-        clicks_ci = confidence_interval(clicks)
-        print(f'Clicks: {np.mean(clicks)}+-{clicks_ci}')
+    for path, rec in zip(recos_paths, recos):
+        print('Computing metrics for path:', path)
 
         precision = test_evaluator.compute_all_precisions(rec)
         precision_ci = confidence_interval(precision)
-        print(f'Precision: {np.mean(precision)}+-{precision_ci}')
+        print(f'Precision: {round(np.mean(precision) * 100, 2)}+-{round(precision_ci * 100, 2)} %')
+
+        recall = test_evaluator.compute_all_recalls(rec)
+        recall_ci = confidence_interval(recall)
+        print(f'Recall: {round(np.mean(recall) * 100, 2)}+-{round(recall_ci * 100, 2)} %')
 
         r_precision = test_evaluator.compute_all_R_precisions(rec)
         r_precision_ci = confidence_interval(r_precision)
-        print(f'R-Precision: {np.mean(r_precision)}+-{r_precision_ci}')
+        print(f'R-Precision: {round(np.mean(r_precision) * 100, 2)}+-{round(r_precision_ci * 100, 2)} %')
+
+        ndcg = test_evaluator.compute_all_ndcgs(rec)
+        ndcg_ci = confidence_interval(ndcg)
+        print(f'NDCG: {round(np.mean(ndcg) * 100, 2)}+-{round(ndcg_ci * 100, 2)} %')
+
+        clicks = test_evaluator.compute_all_clicks(rec)
+        clicks_ci = confidence_interval(clicks)
+        print(f'Clicks: {round(np.mean(clicks), 2)}+-{round(clicks_ci, 2)}')
 
         popularity = test_evaluator.compute_norm_pop(rec)
         popularity_ci = confidence_interval(popularity)
-        print(f'Popularity: {np.mean(popularity)}+-{popularity_ci}')
+        print(f'Popularity: {round(np.mean(popularity) * 100, 2)}+-{round(popularity_ci * 100, 2)} %')
 
         coverage = test_evaluator.compute_cov(rec)
-        print(f'Coverage: {coverage * 100}%')
+        print(f'Coverage: {round(coverage * 100, 2)}%')

src/data_manager/data_manager.py
@@ -71,12 +71,14 @@ class DataManager():
 
         self.track_id_to_album_uri = {track_info['id']: track_info['album_uri'].split(':')[-1]
                                       for track_info in self.tracks_info.values()}
-        self.album_covers_embs = torch.Tensor(np.load(Path(self.foldername) / "album_covers_embeddings" / f'{albums_covers_embs_algorithm}_all_embs.npy'))
-        self.album_uri_to_album_emb_idx = {album_uri: idx for idx, album_uri in enumerate(pd.read_csv(
-            Path(self.foldername) / "album_covers_embeddings" / f'album_uris_{albums_covers_embs_algorithm}.csv',
-            header=None, names=['album_uri']
-        )['album_uri'].tolist())}
-        self.songs_album_cover_embs = self.get_songs_album_cover_embs()
+
+        if albums_covers_embs_algorithm is not None:
+            self.album_covers_embs = torch.Tensor(np.load(Path(self.foldername) / "album_covers_embeddings" / f'{albums_covers_embs_algorithm}_all_embs.npy'))
+            self.album_uri_to_album_emb_idx = {album_uri: idx for idx, album_uri in enumerate(pd.read_csv(
+                Path(self.foldername) / "album_covers_embeddings" / f'album_uris_{albums_covers_embs_algorithm}.csv',
+                header=None, names=['album_uri']
+            )['album_uri'].tolist())}
+            self.songs_album_cover_embs = self.get_songs_album_cover_embs()
 
     def get_songs_album_cover_embs(self):
         album_covers_embs = []